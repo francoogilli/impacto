2fe487620e1176492a24463469049e0a22ef17c4
Franco Gilli <francogilli10@gmail.com>
2024-04-20 20:38:14 -0300

    Add basic layout prototype

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
+// layout.tsx
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+// page.tsx
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className="relative flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="absolute inset-0 z-0 overflow-hidden">
+        <video
+          src="/background.mp4"
+          autoPlay
+          muted
+          playsInline
+          className="w-full h-full object-cover"
+          style={{ width: "125%", height: "125%" }}
+        />
+      </div>
+      <div className="relative z-10">
+        <p className="text-5xl md:text-6xl font-semibold text-[#EFEFEF]">Optimizando tu Empresa con las mejores <span className="text-balance text-[#FFEE00]">Soluciones.</span></p>
+      </div>
     </main>
   );
 }
