ef3756e88c58502b8c589b8cae757aee55e7cffe
Franco Gilli <francogilli10@gmail.com>
2024-04-20 20:59:27 -0300

    Add max width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="relative flex min-h-screen flex-col items-center justify-between px-7 py-4">
       <div className="absolute inset-0 z-0 overflow-hidden">
         <video
           src="/background.mp4"
@@ -14,8 +14,8 @@ export default function Home() {
           style={{ width: "125%", height: "125%" }}
         />
       </div>
-      <div className="relative z-10">
-        <p className="text-5xl md:text-6xl font-semibold text-[#EFEFEF]">Optimizando tu Empresa con las mejores <span className="text-balance text-[#FFEE00]">Soluciones.</span></p>
+      <div className="relative z-10 max-w-[58rem]">
+        <p className="text-5xl md:text-6xl font-semibold text-[#EFEFEF]">Optimiza tu Empresa con las mejores <span className="text-balance text-[#FFEE00]">Soluciones.</span></p>
       </div>
     </main>
   );
