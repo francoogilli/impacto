1901759e43763ab9fe01dee48f2cedf40380e2a6
Franco Gilli <francogilli10@gmail.com>
2024-04-21 16:15:45 -0300

    Add new courses message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,15 @@ export default function Home() {
       </div>
 
       <div className="relative z-10 max-w-[58rem]">
-        <div className="flex justify-center items-center pb-16">
+        <div className="flex justify-center items-center pb-14">
           <Image src="/logo.webp" alt="Logo" width={92} height={92}/>
+        </div>
+
+        <div className="flex justify-center items-center bg-neutral-900 max-w-[14rem] py-2 px-5 mb-4 gap-x-3 rounded-xl mx-auto">
+          <div style={{ width: "10px", height: "10px", backgroundColor: "#4492FC", borderRadius: "50%", boxShadow: "0px 0px 5px 2px #4492FC90"}}></div>
+          <div>
+            <p className="text-white font-medium">¡Nuevos cursos! 🎉</p>
+          </div>
         </div>
         <p className="text-[2.55rem] leading-[1] md:text-[3.55rem] md:leading-[1.2]  font-semibold text-center text-[#EFEFEF]">Potencia tu empresa con las mejores 
           <span className="text-balance text-[#4492FC]"> soluciones.</span>
