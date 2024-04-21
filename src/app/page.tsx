70d85034f83c87966b472377f4e64f75dabd82bf
Franco Gilli <francogilli10@gmail.com>
2024-04-21 15:30:35 -0300

    Add logo and subtitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-between px-7 py-4">
+    <main className="relative flex min-h-screen flex-col items-center justify-between px-7 py-20">
       <div className="absolute inset-0 z-0 overflow-hidden">
         <video
           src="/background.mp4"
@@ -14,8 +14,14 @@ export default function Home() {
           style={{ width: "125%", height: "125%" }}
         />
       </div>
+
       <div className="relative z-10 max-w-[58rem]">
-        <p className="text-5xl md:text-6xl font-semibold text-[#EFEFEF]">Optimiza tu Empresa con las mejores <span className="text-balance text-[#FFEE00]">Soluciones.</span></p>
+        <div className="flex justify-center items-center pb-16">
+          <Image src="/logo.webp" alt="Logo" width={92} height={92}/>
+        </div>
+        <p className="text-[2.55rem] leading-[1] md:text-[3.55rem] md:leading-[1.2]  font-semibold text-center text-[#EFEFEF]">Potencia tu empresa con las mejores 
+          <span className="text-balance text-[#4492FC]"> soluciones.</span>
+        </p> 
       </div>
     </main>
   );
